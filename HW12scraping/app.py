7b02d014d23a7934ecb1757b8e5e37420f22fb1d
Mikeb <mikeb515@gmail.com>
2019-03-03 17:16:45 -0800

    hw 12 sub

---
+++ HW12scraping/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+app.config['MONGO_URI']= 'mongod://localhost:271017/mars_app'
+
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template('index.html', mars=mars)
+
+@app.route("/scrape")
+def scrape():
+    mars = mondo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.update({}, mars_data, upsert= True)
+    return "Scraping Sucess!"
+
+
+if __name__ ==  "__main__":
+    app.run()
