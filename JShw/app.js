ff1f0361b0933000f5ccb6bdafbcd3eda9953c88
Mikeb <mikeb515@gmail.com>
2019-03-08 13:14:04 -0800

    JS HW sub

---
+++ JShw/app.js
@@ -0,0 +1,31 @@
+var tableData = data;
+var tbody = d3.select('tbody');
+
+function buildTable(data) {
+    tbody.html('');
+
+    data.forEach((datarow) => {
+        var row = tbody.append('tr');
+        Object.values(datarow).forEach((val) => {
+            var cell = row.append('td');
+            cell.text(val);
+        });
+    });
+}
+
+function handleClick() {
+
+    d3.event.preventDefault();
+
+    var date = d3.select('#datetime').property('value');
+    let filteredData = tableData;
+
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date)
+    }
+    buildTable(filteredData)
+}
+
+d3.selectAll('filter-btn').on('click', handleClick);
+
+buildTable(tableData);
